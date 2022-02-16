ef50bf34feb7447635cfba7754159870a8fa5014
Waqar <waqar.abid@asktes.com>
2022-02-16 16:28:11 +0500

    Passport and Cookies

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5"); // for hashing passwords
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Setup session
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+	// cookie: { secure: true }
+}));
+
+// Setup passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // connect to mongoose database
 mongoose.connect("mongodb://localhost:27017/userDB", {
@@ -20,11 +36,20 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 // create user schema
 const userSchema = new mongoose.Schema({
 	email: String,
-	password: String,
+	password: String
 });
+
+// create passport local mongoose strategy
+userSchema.plugin(passportLocalMongoose);
 
 // create user model
 const User = new mongoose.model("User", userSchema);
+
+// use passport local mongoose to serialize and deserialize user
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
 	res.render("home");
@@ -38,42 +63,53 @@ app.get("/register", function (req, res) {
 	res.render("register");
 });
 
+// Setup "secrets" route
+app.get("/secrets", function (req, res) {
+	// check if user is logged in
+	if (req.isAuthenticated()) {
+		res.render("secrets");
+	} else {
+		res.redirect("/login");
+	}
+});
+
+// setup logout route
+app.get("/logout", function (req, res) {
+	req.logout();
+	res.redirect("/");
+});
+
+// setup register route
 app.post("/register", function (req, res) {
-	const newUser = new User({
-		email: req.body.username,
-		password: md5(req.body.password),
-	});
-	console.log(newUser);
-	newUser.save(function (err) {
+	User.register({ username: req.body.username}, req.body.password, function (err, user) {
 		if (err) {
 			console.log(err);
+			res.redirect("/register");
 		} else {
-			console.log("saved succesfully");
-			res.render("secrets");
+			passport.authenticate("local")(req, res, function () {
+				res.redirect("/secrets");
+			});
 		}
 	});
 });
 
-// Post to login route
+// setup login route
 app.post("/login", function (req, res) {
-	const username = req.body.username; /* get the username from the form */
-	const password = md5(req.body.password); /* get the password from the form */
-
-	// match the username and password
-	User.findOne({ email: username }, function (err, foundUser) {
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+	req.login(user, function (err) {
 		if (err) {
 			console.log(err);
 		} else {
-			if (foundUser) {
-				// check if the password is correct
-				if (foundUser.password === password) {
-					res.render("secrets");
-				}
-			}
+			passport.authenticate("local")(req, res, function () {
+				res.redirect("/secrets");
+			});
 		}
 	});
 });
 
 app.listen(3000, function () {
 	console.log("Server started at Port 3000");
-});
+})
